33178ad26a7b6769b230c835050d96c90b652c17
Metro Homeschool Robotics <code@metrohomeschoolrobotics.org>
2022-10-27 13:21:02 -0500

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,33 @@
+var html = 
+'    <div class="footer-top">\
+<div class="container">\
+  <div class="row">\
+    <div class="col-lg-3 col-md-6 text-center">\
+      <div class="footer-info">\
+        <div class="social-links mt-3">\
+          <a href="https://www.facebook.com/Metro-Homeschool-Robotics-Team-107266009306531/" class="facebook"><i class="bx bxl-facebook"></i></a>\
+          <a href="https://www.instagram.com/mhr_cyborgs/" class="instagram"><i class="bx bxl-instagram"></i></a>\
+          <a href="https://twitter.com/mhrcyborgs" class="twitter"><i class="bx bxl-twitter"></i></a>\
+          <a href="https://www.youtube.com/user/MetroCyborgs" class="youtube"><i class="bx bxl-youtube"></i></a>\
+        </div>\
+      </div>\
+    </div>\
+  </div>\
+</div>\
+</div>\
+<div class="container">\
+<div class="copyright">\
+  &copy; Copyright <strong><span>Metro Homeschool Robotics Inc</span></strong>. All Rights Reserved\
+</div>\
+<div class="credits">\
+  <!-- All the links in the footer should remain intact. -->\
+  <!-- You can delete the links only if you purchased the pro version. -->\
+  <!-- Licensing information: https://bootstrapmade.com/license/ -->\
+  <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/sailor-free-bootstrap-theme/ -->\
+  Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> &\
+  <br>\
+  <a href="http://horseytech.com/">Horsey Tech</a>\
+</div>\
+</div>';
+
+document.getElementById('footer').innerHTML = html;
---
+++ navbar.js
@@ -0,0 +1,26 @@
+var html = 
+'<ul>\
+<li><a href="index.html">Home</a></li>\
+\
+<li class="dropdown"><a href="about.html"><span>About</span> <i class="bi bi-chevron-down"></i></a>\
+  <ul>\
+    <li><a href="about.html">About</a></li>\
+    <li><a href="testimonials.html">Testimonials</a></li>\
+  </ul>\
+</li>\
+\
+<li><a href="support.html">Support</a></li>\
+\
+<li><a href="media.html">Media</a></li>\
+\
+<li><a href="sponsors.html">Sponsors</a></li>\
+\
+<li><a href="contact.html">Contact</a></li>\
+</ul>\
+<i class="bi bi-list mobile-nav-toggle"></i>';
+
+document.getElementById('navbar').innerHTML = html;
+
+// class="active" example
+// <li><a href="index.html" class="active">Home</a></li>
+// this will make it green when active but only works if nav is done traditionally. 
