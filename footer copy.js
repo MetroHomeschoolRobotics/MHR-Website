6642f2e2bfd4b78166ee50bfbfe81179a77791ec
George Horsey <gchorsey@gmail.com>
2025-05-21 23:56:03 -0500

    tracked footer copy.js

---
+++ footer copy.js
@@ -0,0 +1,16 @@
+var html = 
+'<div class="container">\
+<div class="copyright">\
+  &copy; Copyright <strong><span>Metro Homeschool Robotics Inc</span></strong>. All Rights Reserved\
+</div>\
+<div class="credits">\
+  <!-- All the links in the footer should remain intact. -->\
+  <!-- You can delete the links only if you purchased the pro version. -->\
+  <!-- Licensing information: https://bootstrapmade.com/license/ -->\
+  <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/sailor-free-bootstrap-theme/ -->\
+  Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> &\
+  <br>\
+  <a href="http://horseytech.com/">Horsey Tech</a>\
+</div>';
+
+document.getElementById('footer').innerHTML = html;
